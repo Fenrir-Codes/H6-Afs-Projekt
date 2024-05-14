b8c9f7d663b2638856590f7e4234ce9e77e815a2
Rasmus Knudsen <64476703+rfriisk@users.noreply.github.com>
2024-05-14 13:21:59 +0200

    Small Changes

--- ElevPortalen/Services/AdminService.cs
+++ ElevPortalen/Services/AdminService.cs
@@ -1,4 +1,9 @@
 ﻿using ElevPortalen.Data;
+using ElevPortalen.Models;
+using Microsoft.AspNetCore.DataProtection;
+using Microsoft.EntityFrameworkCore;
+using System.Data;
+using System.Security.Claims;
 
 namespace ElevPortalen.Services
 {
@@ -29,7 +34,45 @@ namespace ElevPortalen.Services
         // Update  ( Company and student update are already working, I'll just note that they are created with a completely different method. )
         //Delete
         //Other functions
+        #region Get registration date
+        public async Task<Dictionary<string, double>> GetMonthlyRegistrationCountsStudent()
+        {
+            var monthlyCounts = await _DataDbcontext.Student
+                .GroupBy(s => new { s.RegisteredDate.Year, s.RegisteredDate.Month })
+                .Select(g => new {
+                    Month = g.Key.Month,
+                    Year = g.Key.Year,
+                    Count = g.Count()
+                })
+                .OrderBy(x => x.Year).ThenBy(x => x.Month)
+                .ToListAsync();
 
+            // Convert month number to month name
+            var monthNames = System.Globalization.CultureInfo.CurrentCulture.DateTimeFormat.MonthNames;
+            return monthlyCounts.ToDictionary(x => monthNames[x.Month - 1] + " " + x.Year, x => (double)x.Count);
+        }
+
+        #endregion
+
+        #region Get registration date
+        public async Task<Dictionary<string, double>> GetMonthlyRegistrationCountsCompany()
+        {
+            var monthlyCounts = await _DataDbcontext.Company
+                .GroupBy(s => new { s.RegisteredDate.Year, s.RegisteredDate.Month })
+                .Select(g => new {
+                    Month = g.Key.Month,
+                    Year = g.Key.Year,
+                    Count = g.Count()
+                })
+                .OrderBy(x => x.Year).ThenBy(x => x.Month)
+                .ToListAsync();
+
+            // Convert month number to month name
+            var monthNames = System.Globalization.CultureInfo.CurrentCulture.DateTimeFormat.MonthNames;
+            return monthlyCounts.ToDictionary(x => monthNames[x.Month - 1] + " " + x.Year, x => (double)x.Count);
+        }
+
+        #endregion
 
     }
 }
